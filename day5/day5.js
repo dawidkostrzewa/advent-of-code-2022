fda7508d98da0d9ba8cbeef815f7ff945dcf9f73
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2022-12-05 17:01:15 +0100

    day5

--- day5/day5.js
+++ day5/day5.js
@@ -27,41 +27,15 @@ fs.readFile('input-5.txt', 'utf8', (err, data) => {
     console.table(cratesGrid)
 
 
-    // console.log(rules.flat())
-    // console.log(Math.max(...rules.flat()))
     const maxColumns = Math.max(...rules.flat())
     const currentColumnsCount = 8
-    const ROWS = 9
 
     const missingColumns = maxColumns - currentColumnsCount
     console.log(missingColumns)
 
     for (let i = 0; i < missingColumns + 6; i++) {
-
         cratesGrid.unshift([0, 0, 0, 0, 0, 0, 0, 0, 0])
     }
-
-
-    // [1,9,2]
-
-    const move = 1;
-    const from = 9;
-    const to = 2;
-
-    // const firstStep = 
-
-    function showAllFromColumn(column) {
-        const columns = []
-        for (let i = 0; i < cratesGrid.length; i++) {
-            // if(cratesGrid[i][] !== "") {
-            columns.push(cratesGrid[i][column])
-            // }
-        }
-
-        // console.log(columns)
-        return columns
-    }
-
 
     function getFromColumn(column, howMany) {
         const columns = []
@@ -78,12 +52,8 @@ fs.readFile('input-5.txt', 'utf8', (err, data) => {
             cratesGrid[notEmptyIndex + i][column] = 0
         }
 
-        // console.log(columns)
         return columns
     }
-
-    // console.log(getFromColumn(1,2))
-    // console.log(showAllFromColumn(1))
 
     function insertToColumn(column, crates) {
         let indexNotFilled = cratesGrid.length;
@@ -106,30 +76,12 @@ fs.readFile('input-5.txt', 'utf8', (err, data) => {
         }
     }
 
-    // console.log(getFromColumn(8))
-    // console.log(showAllFromColumn(8))
 
-    // const dataToInsert = getFromColumn(from-1, move)
-    // insertToColumn(to-1, dataToInsert)
-    // console.table(cratesGrid)
-
- 
     rules.forEach(rule => {
         const [move, from, to] = rule;
         const dataToInsert = getFromColumn(from - 1, move)
         insertToColumn(to - 1, dataToInsert)
     })
 
-    // let ri = 0
-    // setInterval(()=>{
-    //     const [move, from, to] = rules[ri];
-    //     console.log(`move ${move} from ${from} to ${to}`)
-    //     const dataToInsert = getFromColumn(from-1, move)
-    //     insertToColumn(to-1, dataToInsert)
-    //     console.table(cratesGrid)
-    //     ri++
-    // },100)
-
-    console.log("END")
     console.table(cratesGrid)
 });
