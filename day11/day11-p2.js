20c0e34d5a12fd9fb4a9de36952c62cbeae6c2d0
Dawid Kostrzewa <dawid.ko1997@gmail.com>
2022-12-11 21:06:47 +0100

    day11

---
+++ day11/day11-p2.js
@@ -0,0 +1,73 @@
+// TASK: https://adventofcode.com/2022/day/11
+
+const fs = require('fs');
+
+function getResultOfOperation(op, input) {
+    const [operator, value] = op;
+    let intValue = +value;
+    if (value == 'old') {
+        intValue = input;
+    }
+    switch (operator) {
+        case "+":
+            return input + intValue;
+        case "-":
+            return input - intValue;
+        case "*":
+            return input * intValue;
+        case "/":
+            return input / intValue;
+    }
+}
+
+fs.readFile('input-11.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+
+    const monkeys = data.split("Monkey ").map(m => m.split("\n"))
+        .map(x => {
+            const [idx, items, operation, test, conTrue, conFalse] = x;
+            return {
+                idx: +idx.split(":")[0],
+                items: items?.split(":")[1].split(" ").map(x => x.split(",")).flat().filter(x => x !== "").map(x => +x),
+                operation: operation?.split(":")[1].split(" = old")[1].trim().split(" "),
+                test: +test?.split(":")[1].split(" divisible by ")[1],
+                conTrue: +conTrue?.split(":")[1].split("throw to monkey ")[1],
+                conFalse: +conFalse?.split(":")[1].split("throw to monkey ")[1],
+                inspectedTimes: 0
+            }
+        });
+    monkeys.shift();
+
+    const divider = monkeys.map((m) => m.test).reduce((a, b) => a * b, 1);
+    let ROUNDS = 10000;
+
+    while(ROUNDS--) {
+            for (let i = 0; i < monkeys.length; i++) {
+                const monkey = monkeys[i];
+                const { idx, items, operation, test, conTrue, conFalse } = monkey;
+                monkey.inspectedTimes += items.length
+                items.forEach(it => {
+                    const afterOperation = getResultOfOperation(operation, it);
+                    const afterBored = afterOperation % divider;
+                    if (afterBored % test === 0) {
+                        monkeys[conTrue].items.push(afterBored)
+                    }
+                    else {
+                        monkeys[conFalse].items.push(afterBored)
+                    }
+                    return []
+                })
+    
+                monkey.items = [];
+            }
+    }
+
+
+    const mostInspectedMonkeysSorted = monkeys.sort((a, b) => b.inspectedTimes - a.inspectedTimes);
+    const twoMostInspected = mostInspectedMonkeysSorted.slice(0, 2);
+    console.log("PART 2 ANSWER", twoMostInspected[0].inspectedTimes * twoMostInspected[1].inspectedTimes)
+});
